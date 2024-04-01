74cc05107e096667a83510d9410a6f632a618ca7
Rodrigues-Lennon <lennonrodrigues04@gmail.com>
2024-04-01 09:34:56 +0530

    commit 1

---
+++ src/github_practice/Thermos.java
@@ -0,0 +1,44 @@
+package github_practice;
+
+public class Thermos {
+
+	private int volume;
+	private String Brand, Liquid;
+	
+	public Thermos(int v, String b, String l) {
+		setVolume(v);
+		setBrand(b);
+		setLiquid(l);
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public String getBrand() {
+		return Brand;
+	}
+
+	public void setBrand(String brand) {
+		Brand = brand;
+	}
+
+	public String getLiquid() {
+		return Liquid;
+	}
+
+	public void setLiquid(String liquid) {
+		Liquid = liquid;
+	}
+	
+	public void display() {
+		System.out.println("Volume = " + volume + " litre");
+		System.out.println("Brand name is " + Brand);
+		System.out.println("Liquid name is " + Liquid);
+	}
+	
+}
